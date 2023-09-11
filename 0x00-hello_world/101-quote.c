08a97b387d9c69aafe90ab335c0635b0ea066a32
Brian Naimodu <brionaims@gmail.com>
2023-09-11 07:16:50 -0700

    C Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-main(){write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);}
+main(){write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);}
