2a6a00ce46767c5bd641d99691b13beb632f9738
Brian Naimodu <brionaims@gmail.com>
2023-09-11 07:05:57 -0700

    C Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints with proper grammer, but the outcome is a piece of art
+ * Return: always returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
